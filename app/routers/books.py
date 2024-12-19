77bb31621a52327df76c10a370f399a90797497e
Daniel Zadva Jnr <danielzadva@gmail.com>
2024-12-20 00:11:42 +0100

    completed the book crud and operations

--- app/routers/books.py
+++ app/routers/books.py
@@ -11,23 +11,23 @@ async def read_books():
     """returns all books from the schema"""
     return books
 
-@book_router.get("/{book_id}", response_model=Book)
+@book_router.get("/{book_id}", response_model=Book, status_code=200)
 async def read_book(book_id: int):
     """returns a single book from the schema"""
-    for book in books:
+    for book in books.copy():
         if book["id"] == book_id:
             return book
     raise HTTPException(status_code=404, detail="Book not found!")
 
-@book_router.post("", response_model=Book)
-async def create_book(book: BookCreate):
+@book_router.post("", response_model=Book, status_code=201)
+async def create_book(book_in: BookCreate):
     """creates a new book"""
     book_id = len(books) + 1
-    book = Book(**book.model_dump(), id=book_id)
-    books.append(book.model_dump())
-    return {"message": "Book created successfully!", "book": book}
+    new_book = Book(**book_in.model_dump(), id=book_id)
+    books.append(new_book.model_dump())
+    return new_book
 
-@book_router.put("/{book_id}", response_model=Book)
+@book_router.put("/{book_id}", response_model=Book, status_code=200)
 async def update_book(book_id: int, book_in: BookUpdate):
     """updates a book"""
     for book in books:
@@ -38,20 +38,23 @@ async def update_book(book_id: int, book_in: BookUpdate):
 
 @book_router.patch("/{book_id}")
 async def patch_book(book_id: int, book_in: BookUpdate):
-    """patches a book"""
+    """partially updates a book"""
     for book in books:
         if book["id"] == book_id:
-            book.update(book_in.model_dump())
-            return book
+            updated_book = book.copy()
+            updated_book = book_in.model_dump(exclude_unset=True)
+            book.update(updated_book)
+            return updated_book
     raise HTTPException(status_code=404, detail="Book not found!")
 
-@book_router.put("/{book_id}")
-async def mark_book_unavailable(book_id: int, is_available: bool):
+@book_router.patch("/{book_id}/unavailable", response_model=Book)
+async def mark_book_unavailable(book_id: int):
     """Endpoint to mark a book as unavailable"""
-    for book in books:
+    for i, book in enumerate(books):
         if book["id"] == book_id:
-            book["is_available"] = is_available
-            return {"message": "Book marked unavailable!"}
+            books[i]["is_available"] = False
+            # return {"message": "Book marked unavailable!"}
+            return Book(**books[i])
     raise HTTPException(status_code=404, detail="Book not found!")
 
 @book_router.delete("/{book_id}")
